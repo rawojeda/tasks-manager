ba1b9a6205f47f1757acb32255a08e5994981b97
rawojeda <raw_ojeda99@hotmail.com>
2024-07-20 10:22:04 +0200

    starting a proyect from https://start.spring.io

---
+++ src/test/java/com/solid/hexagonal/tasks_manager/TasksManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solid.hexagonal.tasks_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
